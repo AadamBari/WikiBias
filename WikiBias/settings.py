29d1f799c36a22f8ddda5b68ff68fbaa2b130f4e
TheBrownBandit <aadambari@msn.com>
2017-02-13 18:56:08 +0000

    small editions

--- WikiBias/settings.py
+++ WikiBias/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from django.conf import settings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,5 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#     # '/var/www/wikibias.info/public_html/static/',
+# ]
--- WikiBias/urls.py
+++ WikiBias/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^main/', include('main.urls')),
     url(r'^analysis/', include('analysis.urls')),
     url(r'^admin/', admin.site.urls),
+    # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
