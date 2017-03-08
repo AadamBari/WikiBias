8f071ed98ffb4e85f63ba153484aa74fb155ecc9
TheBrownBandit <aadambari@msn.com>
2017-03-08 17:08:05 +0000

    modularised code (retrieve number of watchers function, applied to both language articles)

--- analysis/views.py
+++ analysis/views.py
@@ -2,36 +2,42 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request, article, thepageid, article_two, thepageid_two):
+def index(request, article, thepageid, article_two, thepageid2):
 
     pages = article['query']['pages']
     pages2 = article_two['query']['pages']
 
-    # get article page length in bytes
+    # page length
     length = article_length(pages, thepageid)
-    length2 = article_length(pages2, thepageid_two)
+    length2 = article_length(pages2, thepageid2)
 
-    # check to see if watcher data in json (sometimes not available if too few watchers)
-    if 'watchers' in pages[thepageid]:
-        watchers = pages[thepageid]['watchers']
-
-    else:
-        watchers = "there are 30 watchers or less"
-
-
+    # number of watchers
+    watchers1 = article_watchers(pages, thepageid)
+    watchers2 = article_watchers(pages2, thepageid2)
 
     context = {
         "length": length,
         "length2": length2,
-        "watchers": watchers,
+        "watchers1": watchers1,
+        "watchers2": watchers2,
     }
 
     return render(request, 'analysis/index.html', context)
 
-def article_length(pages,pageid):
+def article_length(pages, pageid):
     """ get article page length in bytes """
 
     length = pages[pageid]['length']
 
     return length
 
+def article_watchers(pages, pageid):
+    """ check to see if watcher data available and return it (sometimes not available if too few watchers) """
+
+    if 'watchers' in pages[pageid]:
+        watchers = pages[pageid]['watchers']
+
+    else:
+        watchers = "there are 30 watchers or less"
+
+    return watchers
